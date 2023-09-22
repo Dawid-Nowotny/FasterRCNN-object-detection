6b494648a6ff43ddaf288bf2881a20dcf078f335
Dawid Nowotny <dawidnowotny@o2.pl>
2023-09-22 12:48:29 +0200

    Fixed function to load model

--- src/load_model.py
+++ src/load_model.py
@@ -1,10 +1,12 @@
 import torch
 
+from .create_fasterrcnn_mobilenet_v3_large_320_fpn import create_fasterrcnn_mobilenet_v3_large_320_fpn
 from .config import MODELS_PATH
 
 def load_model(name):
     try:
-        model = torch.load(f"{MODELS_PATH}/{name}.pth")
+        model = create_fasterrcnn_mobilenet_v3_large_320_fpn()
+        model.load_state_dict(torch.load(f"{MODELS_PATH}/{name}.pth"))
         return model
     except FileNotFoundError:
         raise FileNotFoundError(f"The model '{name}' was not found in {MODELS_PATH}")

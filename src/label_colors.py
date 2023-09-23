b1a5ee233bb66c13f752fe84418a0f100a46c43d
Dawid Nowotny <dawidnowotny@o2.pl>
2023-09-23 12:30:15 +0200

    Added label colors

---
+++ src/label_colors.py
@@ -0,0 +1,22 @@
+label_colors = {
+    'aeroplane': (1.0, 0.0, 0.0),      # Red
+    'bicycle': (0.0, 1.0, 0.0),        # Green
+    'bird': (0.0, 0.0, 1.0),           # Blue
+    'boat': (1.0, 1.0, 0.0),           # Yellow
+    'bottle': (1.0, 0.0, 1.0),         # Magenta
+    'bus': (0.0, 1.0, 1.0),            # Cyan
+    'car': (0.5, 0.0, 0.0),            # Maroon
+    'cat': (0.0, 0.5, 0.0),            # Green
+    'chair': (0.0, 0.0, 0.5),          # Navy
+    'cow': (0.5, 0.5, 0.0),            # Olive
+    'diningtable': (0.5, 0.0, 0.5),    # Purple
+    'dog': (0.0, 0.5, 0.5),            # Teal
+    'horse': (0.5, 0.5, 0.5),          # Gray
+    'motorbike': (0.25, 0.0, 0.0),     # Dark Red
+    'person': (0.0, 0.25, 0.0),        # Dark Green
+    'pottedplant': (0.0, 0.0, 0.25),   # Dark Blue
+    'sheep': (0.25, 0.25, 0.0),        # Dark Yellow
+    'sofa': (0.25, 0.0, 0.25),         # Dark Magenta
+    'train': (0.0, 0.25, 0.25),        # Dark Cyan
+    'tvmonitor': (0.75, 0.75, 0.75)    # Light Gray
+}

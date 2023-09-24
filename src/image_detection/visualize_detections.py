e6612475709e3679898649ec805fa8606ae0a5b6
Dawid Nowotny <dawidnowotny@o2.pl>
2023-09-24 12:53:46 +0200

    Updated detection visualization function

--- src/image_detection/visualize_detections.py
+++ src/image_detection/visualize_detections.py
@@ -1,5 +1,4 @@
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+import cv2
 import numpy as np
 
 from src.label_colors import label_colors
@@ -7,8 +6,7 @@ from src.label_colors import label_colors
 def visualize_detections(image, detections):
     image_np = np.array(image)
 
-    _, ax = plt.subplots(1, figsize=(6, 4))
-    ax.imshow(image_np)
+    image_rgb = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
 
     for detection in detections:
         label = detection['label']
@@ -16,11 +14,24 @@ def visualize_detections(image, detections):
         score = detection['score']
         
         color = label_colors.get(label, (0, 0, 0))
-        rect = patches.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1], linewidth=2, edgecolor=color, facecolor='none')
-        ax.add_patch(rect)
+
+        x, y, x2, y2 = map(int, box)
+
+        label_text = f'{label} {score}'
+        (text_width, text_height), _ = cv2.getTextSize(label_text, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)
         
-        label_text = f'{label} {score})'
-        plt.text(box[0], box[1], label_text, fontsize=8, color='white', bbox=dict(facecolor=color, alpha=0.5))
-    
-    plt.axis('off')
-    plt.show()
+        text_window_height = text_height + 10
+        text_window_width = text_width + 10
+        
+        text_x = x
+        text_y = y - text_window_height if y >= text_window_height else y2 + 10
+        
+        cv2.rectangle(image_rgb, (x, y), (x2, y2), color, 2)
+        
+        cv2.rectangle(image_rgb, (text_x, text_y), (text_x + text_window_width, text_y + text_window_height), color, -1)
+        
+        cv2.putText(image_rgb, label_text, (text_x + 5, text_y + text_height + 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+    cv2.imshow('image', image_rgb)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()

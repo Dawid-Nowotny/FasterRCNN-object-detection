c7268d7ede0f31e733a2dae6a8ff5cc953a56e01
Dawid Nowotny <dawidnowotny@o2.pl>
2023-09-27 14:12:05 +0200

    Added functions to create Fast R-CNN models with MobileNetV3 and ResNet50 backbones

---
+++ src/models/create_fasterrcnn_mobilenet_v3_large_fpn.py
@@ -0,0 +1,14 @@
+from torchvision.models.detection import fasterrcnn_mobilenet_v3_large_fpn
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+from ..config import NUM_CLASSES
+
+def create_fasterrcnn_mobilenet_v3_large_fpn():
+    model = fasterrcnn_mobilenet_v3_large_fpn(
+        weights=None
+    )
+    
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, NUM_CLASSES) 
+
+    return model

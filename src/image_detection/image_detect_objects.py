d584ef1040bb93282582eb336a71c70cf3f8b1fb
Dawid Nowotny <dawidnowotny@o2.pl>
2023-09-27 12:13:24 +0200

    Added NMS and score threshold to object detection function

--- src/image_detection/image_detect_objects.py
+++ src/image_detection/image_detect_objects.py
@@ -1,9 +1,10 @@
 import torch
 from torchvision.transforms import functional as F
+from torchvision.ops import nms
 
 from .label_mapping import label_mapping
 
-def image_detect_objects(model, image, iou_threshold=0.5, use_cuda=True):
+def image_detect_objects(model, image, iou_threshold=0.2, score_threshold=0.6, use_cuda=True):
     device = torch.device("cuda" if use_cuda and torch.cuda.is_available() else "cpu")
     model.eval()
     model.to(device)
@@ -19,10 +20,15 @@ def image_detect_objects(model, image, iou_threshold=0.5, use_cuda=True):
     labels_res = predictions[0]['labels']
     scores_res = predictions[0]['scores']
 
-    selected_indices = scores_res > iou_threshold
+    selected_indices = scores_res > score_threshold
     selected_boxes = boxes_res[selected_indices]
     selected_labels = labels_res[selected_indices]
     selected_scores = scores_res[selected_indices]
+
+    keep_indices = nms(selected_boxes, selected_scores, iou_threshold)
+    selected_boxes = selected_boxes[keep_indices]
+    selected_labels = selected_labels[keep_indices]
+    selected_scores = selected_scores[keep_indices]
 
     results = []
     for box, label, score in zip(selected_boxes, selected_labels, selected_scores):

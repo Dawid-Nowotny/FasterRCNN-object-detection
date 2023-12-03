2a9563c8a28239a0e3c3736e45770880d1f170b7
Dawid Nowotny <dawidnowotny@o2.pl>
2023-12-03 16:00:38 +0100

    Fixed name

--- src/video_detection/process_video.py
+++ src/video_detection/process_video.py
@@ -28,14 +28,14 @@ def process_video(model, video, iou_threshold=0.5, score_threshold=0.6, use_cuda
             score = detection['score']
 
             if box is not None:
-                x, y, w, h = box
+                x, y, x2, y2 = box
                 color = label_colors.get(label, (0, 0, 0))
                 
                 text_x, text_y = x, y - 10
                 if text_y < 10:
-                    text_y = y + h + 20
+                    text_y = y2 + 20
                 
-                frame = cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                frame = cv2.rectangle(frame, (x, y), (x2, y2), color, 2)
                 frame = cv2.putText(frame, f'{label} {score}', (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1.2, color, 5)
 
         frames.append(frame)

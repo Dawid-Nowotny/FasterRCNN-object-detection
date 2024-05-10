b87e3c65e3bc304f5513c37795af91bf5507c857
Dawid Nowotny <dawidnowotny@o2.pl>
2024-05-10 15:55:43 +0200

    Added language selection via command line flag

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import argparse
+
 from src.ui.initialize_app import initialize_app
 
 if __name__ == "__main__":
-    initialize_app()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--en', action='store_true', help='Set language to English')
+    args = parser.parse_args()
+
+    if args.en:
+        language = 'en'
+    else:
+        language = 'pl'
+    
+    initialize_app(language)
